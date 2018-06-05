8e834b65fdb293c2cfcc3ff8d060198d99cdb1ac
Lyuben Dimitrov <lyuben.dimitrov.ext@pentasys.de>
2018-06-05 14:34:12 +0200

    enabled discovery client

--- src/main/java/com/ldimitrov/beercatalogservice/BeerCatalogServiceApplication.java
+++ src/main/java/com/ldimitrov/beercatalogservice/BeerCatalogServiceApplication.java
@@ -2,7 +2,9 @@ package com.ldimitrov.beercatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BeerCatalogServiceApplication {
 
