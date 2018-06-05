ec53de31fc16c8fdc1e81db4bc9a66aabf16f7ce
Lyuben Dimitrov <lyuben.dimitrov.ext@pentasys.de>
2018-06-05 13:59:16 +0200

    Initial commit

---
+++ src/main/java/com/ldimitrov/beercatalogservice/BeerCatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ldimitrov.beercatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerCatalogServiceApplication.class, args);
+	}
+}
